d82d6816ec17f1ecde213f196fdf8ba1245766bc
Przemysław Wierucki <wierucki.p@gmail.com>
2021-02-03 22:17:16 +0100

    Add responsiveness

--- src/router.js
+++ src/router.js
@@ -11,6 +11,6 @@ export const router = new VueRouter({
     },
     routes: [
       { path: '/', component: Home },
-      { path: '/profile', component: Profile }
+      { path: '/profile/:token', component: Profile }
     ]
 });
