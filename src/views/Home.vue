b319b54a36ef7673ba283bf28ad6d714494f7444
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-04-27 17:11:31 -0700

    Revise main popup @mouseleave

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,13 @@
 
         <p style="color: #0e314c; font-size: medium;font-weight: bold">How about VIP access for free?</p>
         <div class="row">
-          <div class="col-6">
-            <p style="color: #6084a4; font-size: medium">Fill out a 6-question survey about online tutoring</p>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfVN4XavrzDgfg8XE3guult0aNoztYdFbXZs-7bL0EWc0aamQ/viewform" class="btn btn-primary mt-5 d-block mx-auto" style="text-transform:none;">Claim VIP access</a>
+          <div class="col-7">
+            <p style="color: #6084a4; font-size: 13px">We are looking for <span style="font-weight: bold">20 tutors</span> to test our new website dashboard.</p>
+            <p style="color: #6084a4; font-size: 13px; margin-bottom: 0px;">Get FREE website presence for 3 months.</p>
+            <p style="color: #6084a4; font-size: 13px; margin-bottom: 0px;">Contact us for details.</p>
+            <a href="mailto:ceo@tutomy.com?subject=Testing website dashboard" class="btn btn-primary mt-5 d-block mx-auto" style="text-transform:none;">Claim VIP access</a>
           </div>
-          <div class="col-6">
+          <div class="col-5">
             <img src="./../assets/img/survey.svg" alt="video-pic">
           </div>
         </div>
