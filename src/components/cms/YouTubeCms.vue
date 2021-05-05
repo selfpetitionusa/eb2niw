96ae40deb36c9cfbc1b6a6cce38f6f854470ef97
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-05-04 22:04:50 -0700

    Add a href to plus YT button

--- src/components/cms/YouTubeCms.vue
+++ src/components/cms/YouTubeCms.vue
@@ -1,7 +1,9 @@
 <template>
 
     <div class="YT-cms">
-        <font-awesome-icon class="fas fa-plus-circle fa-3x" icon="plus-circle" />
+        <a href="./#">
+            <font-awesome-icon class="fas fa-plus-circle fa-3x" icon="plus-circle" />
+        </a>
         <p>Do you have a YouTube video?</p>
         <p>Show parents and students your value and tutoring style.</p>
     </div>
