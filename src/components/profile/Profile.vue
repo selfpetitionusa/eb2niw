e51debe74c8922b1cc2d3b73056e84e6c4e861d3
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-06-02 15:19:31 -0700

    Fix flow: background & hamburger menu

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -3,27 +3,28 @@
     <div class="dflex">
         <div :id="response.profile.token" v-if="response.profile.token" v-show="false"></div>
 
-<!-- HAMBURGER MENU -->
-
-    <div class="hamburger-nav">
-        <div id="myLinks">
-            <a href="#section-about-me" v-on:click="actionHamburgerMenu">About me</a>
-            <a href="#section-subjects" v-on:click="actionHamburgerMenu">Subjects</a>
-            <a href="#section-rates" v-on:click="actionHamburgerMenu">Rates</a>
-            <a href="#section-expertise" v-if="!(response.profile.youtubeIntroLink === null && response.problemCards.length === 0)" v-on:click="actionHamburgerMenu">Expertise</a>
-        </div>
-
-        <a v-on:click="actionHamburgerMenu" class="icon">
-            <i class="fa fa-bars"></i>
-        </a>
-    </div>
-
 
 <!-- PROFILE TEMPLATE -->
 
     <div class="background-rectangular"></div>
     <div id="tu-container" class="tu-container">
     <div class="row website-split">
+
+
+<!-- HAMBURGER MENU -->
+
+        <div class="hamburger-nav">
+            <div id="myLinks">
+                <a href="#section-about-me" v-on:click="actionHamburgerMenu">About me</a>
+                <a href="#section-subjects" v-on:click="actionHamburgerMenu">Subjects</a>
+                <a href="#section-rates" v-on:click="actionHamburgerMenu">Rates</a>
+                <a href="#section-expertise" v-if="!(response.profile.youtubeIntroLink === null && response.problemCards.length === 0)" v-on:click="actionHamburgerMenu">Expertise</a>
+            </div>
+
+            <a v-on:click="actionHamburgerMenu" class="icon">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
 
 
 <!-- LEFT SECTION -->
