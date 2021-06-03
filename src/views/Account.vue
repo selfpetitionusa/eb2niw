4bc8d5932355c3a5c2d0685c56cacbd1f1868b2c
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-06-03 13:32:39 -0700

    Update url for account (basic-info & profile-link)

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -8,8 +8,8 @@
                         <div class="row">
                             <div class="col-3">
                                 <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                    <router-link class="nav-link" id="v-pills-link-tab" data-toggle="pill" role="tab" aria-controls="v-pills-link"  :to="{ path: '/account/profile-link' }">Your Website Address</router-link>
-                                    <router-link class="nav-link" id="v-pills-home-tab" data-toggle="pill" role="tab" aria-controls="v-pills-basic"  :to="{ path: '/account/basic-info' }">Registration Info</router-link>
+                                    <router-link class="nav-link" id="v-pills-link-tab" data-toggle="pill" role="tab" aria-controls="v-pills-link"  :to="{ path: '/account/website-link' }">Your Website Address</router-link>
+                                    <router-link class="nav-link" id="v-pills-home-tab" data-toggle="pill" role="tab" aria-controls="v-pills-basic"  :to="{ path: '/account/registration-info' }">Registration Info</router-link>
                                     <router-link class="nav-link" id="v-pills-reset-tab" data-toggle="pill" role="tab" aria-controls="v-pills-password"  :to="{ path: '/account/password-reset' }">Password Reset</router-link>
                                 </div>
                             </div>
