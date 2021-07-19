9229d4f6a78b5bcee1aaf1b01e1c17d508cbf6df
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-07-19 15:49:39 -0700

    Add testimonials section

--- src/components/home/Testimonials.vue
+++ src/components/home/Testimonials.vue
@@ -6,9 +6,14 @@
                 <h2>What users say?</h2>
                 <div class="bar"></div>
                 <div class="paragraph">
-                    <p>Don't take our word for it - check what customers think</p>
+                    <p>Don't take our word for it - check what it is all about</p>
                 </div>
             </div>
+        </div>
+
+        <div style="background: linear-gradient(135deg, #23bdb8 0%, #43e794 100%);">
+            <h3>Join 1,300 private tutors who use Tutomy</h3>
+            <h5>Find out how to leverage professional tutoring website to find students</h5>
         </div>
     </section>
 </template>
