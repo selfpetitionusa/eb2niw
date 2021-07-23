40ce029a03fd6a149c48a87901c7f271959e3321
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-07-22 17:50:50 -0700

    Simplify website assessment

--- src/views/NeedsAssessment.vue
+++ src/views/NeedsAssessment.vue
@@ -7,30 +7,29 @@
                 </div>
 
                 <div class="col-sm-6 offset-sm-1">
-                    <h2>Test your website needs</h2>
-                    <p><span class="desc">If you don't have a website yet </span>- quiz will tell you what type of website do you need and which resources would be the most useful</p>
-                    <p><span class="desc">If you already have a website </span>- quiz will tell you how well your website is performing, and what you could improve</p>
+                    <div class="row">
+                        <div class="col-10">
+                            <h2>Test your website needs</h2>
+                            <p>Based on your responses, find out about the best tools to create a website, ways to build an online presence, and find students</p>
 
-                    <form id="website-assessment" @submit.prevent="addEmail(email)" class="mt-2">
-                        <div class="row">
-                            <div class="col-10 assessment-choice">
-                                <div class="title">Choose one option:</div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="noWebsite" id="noWebsite" value="noWebsite">
-                                    <label class="form-check-label" for="noWebsite">I do NOT have a tutoring website</label>
+                            <form id="website-assessment" @submit.prevent="addEmail(email)">
+                                <div class="assessment-choice">
+                                    <div class="title">Choose one option:</div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="noWebsite" id="noWebsite" value="noWebsite">
+                                        <label class="form-check-label" for="noWebsite">I do NOT have a tutoring website</label>
+                                    </div>
+                                    <div class="form-check form-inline" style="display: flex; align-items: center; padding: 0px">
+                                        <input class="form-check-input" type="radio" name="yesWebsite-radio" id="yesWebsite" value="yesWebsite" style="display: flex; align-items: center">
+                                        <label class="form-check-label" for="yesWebsite">I have a tutoring website:</label>
+                                        <input type="text" class="form-control" id="yesWebsite" disabled placeholder="Link to your website" style="height: 30px; width: 51%; margin-left: 10px; border-color: #0e314c;">
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="buildingWebsite" id="buildingWebsite" value="buildingWebsite">
+                                        <label class="form-check-label" for="buildingWebsite">I am in the process of building a tutoring website</label>
+                                    </div>
                                 </div>
-                                <div class="form-check form-inline" style="display: flex; align-items: center; padding: 0px">
-                                    <input class="form-check-input" type="radio" name="yesWebsite-radio" id="yesWebsite" value="yesWebsite" style="display: flex; align-items: center">
-                                    <label class="form-check-label" for="yesWebsite">I have a tutoring website:</label>
-                                    <input type="text" class="form-control" id="yesWebsite" disabled placeholder="Link to your website" style="height: 30px; width: 51%; margin-left: 10px; border-color: #0e314c;">
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="buildingWebsite" id="buildingWebsite" value="buildingWebsite">
-                                    <label class="form-check-label" for="buildingWebsite">I am in the process of building a tutoring website</label>
-                                </div>
-                            </div>
 
-                            <div class="col-10">
                                 <div>
                                     <input type="text"  name="name" id="name" class="form-control" v-model="name" placeholder="Name">
                                     <div class="mt-2">
@@ -43,15 +42,16 @@
                                         <p class="m-0">{{ message }}</p>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="col-8">
-                                <button style="width: 58%; margin: 0" type="submit" class="btn btn-primary btn-assess">Take a quiz</button>
-                            </div>
+                                <div>
+                                    <button style="width: 58%; margin: 0" type="submit" class="btn btn-primary btn-assess">Take a quiz</button>
+                                </div>
+                            </form>
 
                         </div>
-                    </form>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
