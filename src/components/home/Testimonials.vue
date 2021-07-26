02725e722d979ead999c46c707e4cf67e9c9f04d
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-07-26 16:32:01 -0700

    Update wording for testimonials

--- src/components/home/Testimonials.vue
+++ src/components/home/Testimonials.vue
@@ -6,14 +6,15 @@
                 <h2>What users say?</h2>
                 <div class="bar"></div>
                 <div class="paragraph">
-                    <p>Don't take our word for it - check what Tutomy is all about</p>
+                    <p>From our users -- private tutors, like you.</p>
+                    <p>They all built their business with Tutomy:</p>
                 </div>
             </div>
         </div>
 
         <div style="background: linear-gradient(135deg, #23bdb8 0%, #43e794 100%);">
-            <h3>Join 1,300 private tutors who use Tutomy</h3>
-            <h5>Find out how to leverage professional tutoring website to find students</h5>
+            <h3>Join Tutomy community of 1,300+ private tutors</h3>
+            <h5>Leverage a professional tutoring website to find students!</h5>
         </div>
     </section>
 </template>
