a7d39f1f35370b96e1108ed45d87a69130813b15
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-07-31 14:50:12 -0700

    Fix responsiveness for social media

--- src/components/home/Partner.vue
+++ src/components/home/Partner.vue
@@ -7,15 +7,15 @@
             <div class="partner-inner">
                 <ul class="social-links">
                     <div class="row">
-                        <li class="col-sm-4">
+                        <li class="col-4">
                             <a class="facebook" href="https://www.facebook.com/groups/fortutorsbytutors"><feather type="facebook"></feather></a>
                         </li>
 
-                        <li class="col-sm-4">
+                        <li class="col-4">
                             <a class="youtube" href="https://www.youtube.com/channel/UCPiCaTX8uBdnQzMO4w3QeYA"><feather type="youtube"></feather></a>
                         </li>
 
-                        <li class="col-sm-4">
+                        <li class="col-4">
                             <a class="linkedin" href="https://www.linkedin.cn/company/tutomy/"><feather type="linkedin"></feather></a>
                         </li>
                     </div>
