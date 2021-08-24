e90de41208371a75d8e50be3be2c09836869cb19
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-08-24 16:23:20 -0700

    Make register-confirmation responsive

--- src/views/RegisterConfirmation.vue
+++ src/views/RegisterConfirmation.vue
@@ -1,27 +1,30 @@
 <template>
-    <div class="jumbotron main-banner register-confirmation">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-12 offset-lg-2">
-                    <h1>Welcome to your new website!</h1>
+    <div class="register-confirmation" style="height: 100%">
+        <div class="jumbotron main-banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-12 offset-lg-2">
+                        <h1>Welcome to your new website!</h1>
 
-                    <div style="margin-top: 2rem; margin-bottom: 1rem">
-                        <h6 style="margin-bottom: 0px">Check email to verify your account</h6>
-                        <p>Tip: See spam folder in case the e-mail was incorrectly identified</p>
+                        <div class="div1">
+                            <h6>Check email to verify your account</h6>
+                            <p>Tip: See spam folder in case the e-mail was incorrectly identified</p>
+                        </div>
+
+                        <div class="div2">
+                            <h6>Your website will automatically display information upon completion:</h6>
+                            <p><a href="www.tutomy.com/tutor/test1test2">www.tutomy.com/tutor/test1test2</a></p>
+                        </div>
+
+                        <h6>In the future, you can always login to your account on www.tutomy.com</h6>
+
+                        <div class="div3">
+                            <h4>Click <strong>NEXT</strong> to complete your website in <strong>11min</strong></h4>
+                        </div>
+
+                        <a href="/dashboard" class="btn btn-primary btn-register">NEXT</a>
                     </div>
 
-                    <div style="margin-top: 1rem; margin-bottom: 1rem">
-                        <h6 style="margin-bottom: 0px">Your website will automatically display information upon completion:</h6>
-                        <p><a href="www.tutomy.com/tutor/test1test2">www.tutomy.com/tutor/test1test2</a></p>
-                    </div>
-
-                    <h6>In the future, you can always login to your account on www.tutomy.com</h6>
-
-                    <div style="margin-top: 2.5rem">
-                        <h4>Click <strong>NEXT</strong> to complete your website in <strong>11min</strong></h4>
-                    </div>
-
-                    <a href="/dashboard" class="btn btn-primary btn-register">NEXT</a>
                 </div>
 
 
