12351062a6a14414c2ad0ce99cee48b1c897f942
Przemysław Wierucki <wierucki.p@gmail.com>
2021-12-02 00:52:59 +0100

    Fix payment link

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -134,7 +134,7 @@
                                 <div style="font-size: 12px"> -> Check result in PREVIEW</div>
                             </div>
                         <a v-if="!cmsToggle && response.profile.bookingLink" :href="response.profile.bookingLink" class="btn btn-primary btn-border inner-booking">Book</a>
-                        <a v-if="!cmsToggle && response.profile.paymentLink === 'Payment'" :href="response.profile.paymentLink" class="btn btn-primary btn-border inner-booking">Pay</a>
+                        <a v-if="!cmsToggle && response.profile.paymentLink" :href="response.profile.paymentLink" class="btn btn-primary btn-border inner-booking">Pay</a>
                     </div>
                 </div>
             </div>
