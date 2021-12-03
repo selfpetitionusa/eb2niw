5b2fad1fa7e614f34eda122a8e7b74a057da5c16
Natalia Mackowiak <nat.mackowiak@gmail.com>
2021-12-02 17:56:16 -0800

    Fix overflowing text in rates section

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -358,7 +358,7 @@
                             <div class="col-xl-4 col-lg-4 col-md-3 col-sm-3 col-3 rate-box">
                                 <div class="inner">{{rate.currency}}{{rate.amount}}</div>
                             </div>
-                            <div class="col-xl-8 col-lg-8 col-md-9 col-sm-9 col-9 rate-comment">
+                            <div class="col-xl-8 col-lg-8 col-md-9 col-sm-9 col-8 rate-comment">
                                 <div class="row">
                                     <p class="col top">{{rate.commentTop}}</p>
                                     <div class="w-100"></div>
