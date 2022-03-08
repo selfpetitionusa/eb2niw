e0b0d3c0b98df7fba0fd7c0d4c856aa4cddbb137
Natalia Mackowiak <nat.mackowiak@gmail.com>
2022-03-08 10:23:32 -0800

    Add banner green card

---
+++ src/views/ideas/green-card/components/BannerGreenCard.vue
@@ -0,0 +1,193 @@
+<template>
+    <!-- Start Main Banner -->
+    <div class="main-banner">
+        <SectionOne id="home"/>
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="row h-100 justify-content-center align-items-center">
+                        <div class="col-xl-6 col-lg-7">
+                            <div class="hero-content">
+                                <div class="row">
+                                    <div class="col-lg-11">
+                                        <h1>Dollar voting for influencers</h1>
+                                        <h5>Connect with your audience far more closely and monetize this connection</h5>
+
+                                        <div class="tick-section">
+                                            <div class="d-block d-sm-none">
+                                                <div class="tick-row">
+                                                    <font-awesome-icon class="fa-2x tick-icon" icon="check-square" />
+                                                    <p class="tick-input">Engage subscribers with polls</p>
+                                                </div>
+                                            </div>
+
+                                                <div class="d-none d-sm-block">
+                                                    <div class="tick-row">
+                                                        <font-awesome-icon class="fa-2x tick-icon" icon="check-square" />
+                                                        <p class="tick-input">Engage subscribers with polls</p>
+                                                    </div>
+                                                </div>
+
+                                            <div class="d-block d-sm-none">
+                                                <div class="tick-row">
+                                                    <font-awesome-icon class="fa-2x tick-icon" icon="check-square" />
+                                                    <p class="tick-input">Let them vote with money</p>
+                                                </div>
+                                            </div>
+
+                                                <div class="d-none d-sm-block">
+                                                    <div class="tick-row">
+                                                        <font-awesome-icon class="fa-2x tick-icon" icon="check-square" />
+                                                        <p class="tick-input">Let them vote with money</p>
+                                                    </div>
+                                                </div>
+
+                                            <div class="d-block d-sm-none">
+                                                <div class="tick-row">
+                                                    <font-awesome-icon class="fa-2x tick-icon" icon="check-square" />
+                                                    <p class="tick-input">Deliver outcome that your audience truly desires</p>
+                                                </div>
+                                            </div>
+
+                                                <div class="d-none d-sm-block">
+                                                    <div class="tick-row">
+                                                        <font-awesome-icon class="fa-2x tick-icon" icon="check-square" />
+                                                        <p class="tick-input">Deliver outcome that your audience truly desires</p>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-xl-6 col-lg-5 video">
+                            <h6 class="intro-video">The chance for your fans to vote with money</h6>
+                            <div class="row">
+
+                                <div class="d-block d-lg-none">
+                                    <vue-plyr ref="plyr">
+                                        <video
+                                                controls
+                                                crossorigin
+                                                playsinline
+                                                poster="../../assets/img/thumbnail.png">
+                                            <source src="../../assets/img/Intro-video-Rena.mp4" type="video/mp4">
+                                            Video how to create a tutoring website. Your browser does not support the video tag
+                                        </video>
+                                    </vue-plyr>
+                                </div>
+
+                                <div class="d-none d-lg-block">
+                                    <vue-plyr ref="plyr">
+                                        <video
+                                                controls
+                                                crossorigin
+                                                playsinline
+                                                poster="../../assets/img/thumbnail.png">
+                                            <source src="../../assets/img/Intro-video-Rena.mp4" type="video/mp4">
+                                            Video how to create a tutoring website. Your browser does not support the video tag
+                                        </video>
+                                    </vue-plyr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="row banner-action-btns">
+                        <div class="col-xl-7 col-lg-9">
+                            <div class="row justify-content-center">
+                                <div class="col-sm-8 col-10">
+                                    <a href="/register" @click="$gtag.event('register')" class="btn btn-primary btn-register btn-profile">Let your fans vote with money</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-11">
+                            <p class="bonus1">No payment or credit card is required</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="shape1"><img src="../../assets/img/shape1.png" ></div>
+        <div class="shape3"><img src="../../assets/img/shape3.svg" ></div>
+        <div class="shape4"><img src="../../assets/img/shape4.svg" ></div>
+        <div class="shape5"><img src="../../assets/img/shape5.png" ></div>
+        <div class="shape6 rotateme"><img src="../../assets/img/shape4.svg" ></div>
+        <div class="shape7"><img src="../../assets/img/shape4.svg" ></div>
+        <div class="shape8 rotateme"><img src="../../assets/img/shape2.svg" ></div>
+    </div>
+    <!-- End Main Banner -->
+</template>
+
+
+
+<script>
+
+    export default {
+        name: 'Banner',
+        data() {
+            return {
+                fired: false
+            }
+        },
+        mounted () {
+            this.$refs.plyr.player.volume = 0.5;
+            this.$refs.plyr.player.on('playing', () => {
+                if(!this.fired) {
+                    this.$gtag.event('video')
+                }
+                this.fired = true;
+
+            })
+        }
+    }
+
+
+/* JS FOR SIGNUP
+    data() {
+        return {
+            email: '',
+            message: ''
+        }
+    },
+    methods: {
+         addEmail(email) {
+            if (!email) return;
+            var noticeMessage = "🎉 Your account has been saved  🎉";
+             const url = `/api/lead`;
+             const requestOptions = {
+                 method: "POST",
+                 headers: {"Content-Type" : "application/json"},
+                 body: JSON.stringify({ email: email })
+             };
+             fetch(url, requestOptions).then(async response => {
+                 if (response.ok) {
+                     this.$refs['modal-1'].show();
+                     this.$store.commit('popup/setSeen');
+                 } else {
+                     noticeMessage = "This email address is already registered";
+                 }
+                 this.message = noticeMessage;
+                 this.email = '';
+             });
+        },
+        randomPassword(length) {
+            var result           = '';
+            var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var charactersLength = characters.length;
+            for ( var i = 0; i < length; i++ ) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+        }
+    }
+}
+*/
+
+</script>
