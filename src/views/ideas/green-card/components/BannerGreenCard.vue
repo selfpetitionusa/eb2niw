414f6c0137f13f63be69ad01e0fc129e6d475f5d
Natalia Mackowiak <nat.mackowiak@gmail.com>
2022-06-08 17:30:37 -0700

    Change lead button name

--- src/views/ideas/green-card/components/BannerGreenCard.vue
+++ src/views/ideas/green-card/components/BannerGreenCard.vue
@@ -64,13 +64,13 @@
 
                                             <form v-if="show_contact" id="sign" @submit.prevent="addEmail(email)" class="mt-4">
                                                 <div class="row">
-                                                    <div class="col-sm-8">
+                                                    <div class="col-sm-7">
                                                         <input type="email"  name="greencard-lead" id="greencard-lead" class="form-control"  v-model="email" required data-error="Please enter your email" placeholder="Enter your email address">
                                                         <p class="tick-input" style="font-size: 10px; color:#495057">Discuss your case with us</p>
                                                     </div>
-                                                    <div class="col-sm-4">
+                                                    <div class="col-sm-5">
                                                         <div>
-                                                            <button type="submit" @click="goToEligibility()" class="btn btn-primary">Submit</button>
+                                                            <button type="submit" @click="goToEligibility()" class="btn btn-primary">FIND A LAWYER</button>
                                                           </div>
                                                     </div>
                                                 </div>
