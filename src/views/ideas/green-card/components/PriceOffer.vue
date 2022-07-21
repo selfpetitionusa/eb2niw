ffd95bff002c435b850acb9dd9769e893846ecf4
Natalia Mackowiak <nat.mackowiak@gmail.com>
2022-07-21 16:25:31 -0700

    Hide special offer discount when time runs out

--- src/views/ideas/green-card/components/PriceOffer.vue
+++ src/views/ideas/green-card/components/PriceOffer.vue
@@ -2,9 +2,10 @@
 
     <div class="pricing-table col-md-6 col-11 offset-md-3" style="margin-top: 6rem; padding-top: 1.5rem">
         <div class="price" style="margin-bottom: 0;">
-            <span style="color: #0b5345;"><sup>$</sup>89</span>
+            <span v-if="secondCount > 0" style="color: #0b5345;"><sup>$</sup>89</span>
+            <span v-if="secondCount <= 0" style="color: #0b5345;"><sup>$</sup>149</span>
         </div>
-        <div class="price" style="margin-bottom: 0.5rem;">
+        <div v-show="secondCount > 0" class="price" style="margin-bottom: 0.5rem;">
             <sup style="color: #b30000; font-size: 15px;">$</sup><span style="color: #b30000; font-size: 20px; text-decoration: line-through">149</span>
         </div>
 
@@ -13,12 +14,14 @@
                 <a href="https://checkout.stripe.com/pay/cs_live_a1qNyGjaPCfAQFuj7MBooXupRt0n3gtqfiNtsv7K0zAGkoNEee0B1svkIn#fidkdWxOYHwnPyd1blppbHNgWjA0T290ckNBSGlgSWtqdWJTazBTbnJRPU1kdmBgRlBXVEsyYXQxdURCZlJ3XGlAZnFDdm00cmJgSzJXN21SZjxnT3ZUfGZqaHxmPHVCVXRNVFF1cDc1TmdLNTVgQ0I2QjVATCcpJ3VpbGtuQH11anZgYUxhJz8nMnZMMGtQNXV1NnJ1ZnNgY1xfJ3gl" @click="$gtag.event('register')" class="btn btn-primary">BUY EB-2 NIW STARTER KIT</a>
             </div>
         </div>
-        <div style="margin-top: 1rem; font-size: 12px">Valid for:</div>
-        <div style="font-size: 18px">
-            <span style="font-weight: 600" v-if="days > 0">{{ days }}</span><span v-if="days > 0"> day</span><span v-if="days > 1">s</span>
-            <span style="font-weight: 600"> {{ hours }}</span>h :
-            <span style="font-weight: 600">{{ minutes }}</span>m :
-            <span style="font-weight: 600">{{ seconds }}</span>s
+        <div v-show="secondCount > 0">
+            <div style="margin-top: 1rem; font-size: 12px">Valid for:</div>
+            <div style="font-size: 18px">
+                <span style="font-weight: 600" v-if="days > 0">{{ days }}</span><span v-if="days > 0"> day</span><span v-if="days > 1">s</span>
+                <span style="font-weight: 600"> {{ hours }}</span>h :
+                <span style="font-weight: 600">{{ minutes }}</span>m :
+                <span style="font-weight: 600">{{ seconds }}</span>s
+            </div>
         </div>
         <p style="margin-top: 1rem; line-height: 18px; font-size: 12px">You will receive an email with documents. Please check your spam folder. <br>Templates are provided in electronic form only (pdf and doc files).</p>
     </div>
@@ -33,7 +36,7 @@
         data () {
             return {
               now: Math.trunc((new Date()).getTime() / 1000),
-              event: new Date('2022-07-26T00:00:00'),
+              event: new Date('2022-07-31T00:00:00'),
               finish: false
             }
           },
@@ -70,7 +73,7 @@
               if (this.secondCount < 0) return 0
               return Math.trunc(this.secondCount / 60 / 60 / 24)
             }
-          }
+          },
     }
 
 </script>
