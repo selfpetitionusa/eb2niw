b41579617a967ea429072e36c419cb7f595cc615
Natalia Mackowiak <nat.mackowiak@gmail.com>
2022-07-22 20:41:42 -0700

    Ad meta desc & title

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,14 @@
     },
     metaInfo() {
     return {
-      title: "Self-petition green card for professionals"
+      title: "Self Petition USA - Templates for EB-2 NIW for engineers & business professionals",
+      meta: [
+        { vmid: 'description', name: 'description', content:  'Templates to build your own successful EB-2 National Interest Waiver (NIW) case: actual, successful application with 3 reference letters which led to EB-2 NIW green card by a business professional and instructions to arrange for high-quality reference letters and write a successful applications'},
+        { vmid: 'ogtitle', property: 'og:title', content: "Self Petition USA - Templates for EB-2 NIW for professionals"},
+        { vmid: 'ogsite', property: 'og:site_name', content: 'Self Petition USA'},
+        { vmid: 'ogtype', property: 'og:type', content: 'website'},
+        { vmid: 'ogimage1', property: 'og:image', content: `https://www.selfpetitionusa.com/thumbnail.png`}
+      ]
     }
   }
 }
